6363d7821a1864ac7e6fccedf1a427192fbb715a
Pavlo-Tytko <Pavlo.tytko@vakoms.com>
2021-09-09 17:25:38 +0300

    check if both arguments of hypotenuse are bigger then 0

--- functions.js
+++ functions.js
@@ -1,5 +1,9 @@
 function hypotenuse (firsSide, secondSide) {
+    if(firsSide > 0 && secondSide > 0) {
     return Math.sqrt(Math.pow(firsSide, 2) + Math.pow(secondSide, 2));
+    } else {
+        return null
+    }
 }
 
 module.exports = hypotenuse;
--- functions.test.js
+++ functions.test.js
@@ -9,3 +9,9 @@ test('Find hypotenuse 6 and 8 to be equal to 10', () => {
 test('Find hypotenuse 6 and 9 to be close to 10', () => {
     expect(hypotenuse(6, 9)).toBeCloseTo(10.8, 1);
 });
+test('any 0 as argument needs to return Null', () => {
+    expect(hypotenuse(0, 9)).toBeNull();
+});
+test('any 0 as argument needs to return Null', () => {
+    expect(hypotenuse(6, 0)).toBeNull();
+});
